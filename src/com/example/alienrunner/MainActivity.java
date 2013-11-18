4402f262a8f987104661e2ad80a38f4c6a762485
Jonathan <jonathan.klingberg@gmail.com>
2013-11-18 10:44:21 +0100

    Changed packet name

--- src/com/example/alienrunner/MainActivity.java
+++ src/com/example/alienrunner/MainActivity.java
@@ -1,4 +1,4 @@
-package com.spoton.solutions.androidclient;
+package com.example.alienrunner;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+
+import com.spoton.solutions.androidclient.R;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
